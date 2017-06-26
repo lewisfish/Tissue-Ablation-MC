b790a793df81dd5f82bfcd09a7c9b99bba847355
Lewis McMillan <lewistmcmillan@gmail.com>
2017-06-26 15:58:53 +0100

    finished 3d heat parrallel shizz

--- data/3d_test.f90
+++ data/3d_test.f90
@@ -12,13 +12,10 @@ program ring
     integer :: recv_status(MPI_STATUS_SIZE)
     logical :: periods(1), reorder
 
-    !vars for subarray
-    integer :: starts(3), oldsize(3), newsize(3), subarr
-
     !variables for heat
     integer :: size_x, size_y, size_z, n, p, o, u, numpoints, lo, hi
-    real ::  k0, hx, hx2, hy, hy2, hz, hz2, delt, u_xx, u_yy, u_zz
-    real, allocatable :: tmp(:,:,:)
+    real ::  k0, hx, hx2, hy, hy2, hz, hz2, delt, u_xx, u_yy, u_zz,start, finish
+    real, allocatable :: tmp(:,:,:), out(:,:,:)
 
     !init all mpi shizz
     comm = mpi_comm_world
@@ -40,11 +37,11 @@ program ring
                         !comm, direction, shift, source, dest, error
     call mpi_cart_shift(new_comm, 0, 1, left, right, error)
 
-    numpoints = 40
+    numpoints = 48
 
     if(id == 0)then
         !do decomp
-        if(mod(numpoints, numproc) /= 0)then
+        if(mod(numpoints,numproc) /= 0)then
             print('(I2.1,a,I2.1)'),numpoints,' not divisable by : ', numproc
             call mpi_finalize(error)
             error stop
@@ -56,11 +53,13 @@ program ring
     call mpi_barrier(new_comm, error)
     call MPI_Bcast(N, 1, MPI_integer ,0 , new_comm, error)
 
-!init heat shizz
+    if(id==0)call cpu_time(start)
+
+    !init heat shizz
     k0 = 1.
     size_x = numpoints
-    size_y = N
-    size_z = numpoints
+    size_y = numpoints
+    size_z = N
 
     hx = 1. / (numpoints + 2)
     hy = 1. / (numpoints + 2)
@@ -78,45 +77,48 @@ program ring
     yi = 1
     yf = size_y
 
-    ! yi = id*size_y+1
-    ! if(yi == 0)yi = 1
-    ! yf = yi + size_y-1
-
     zi = 1
     zf = size_z
 
-    ! allocate(tmp(0:n*numproc+1, 0:n*numproc+1, 0:n*numproc+1)) !old
-    allocate(tmp(xi-1:xf+1,yi-1:yf+1,zi-1:zf+1))
-
-    !create mpi sub array to avoid temp array creation
-    oldsize = [size(tmp,1), size(tmp,2), size(tmp,3)]
-    newsize = [[size(tmp,1)-2, 1, size(tmp,3)-2]]
-    starts = [xi, yf, zi]
-
-    print*,oldsize,newsize,starts
-
-    call mpi_type_create_subarray(3, oldsize, newsize, starts, mpi_order_fortran, &
-                                  mpi_real, subarr, error)
-    call mpi_type_commit(subarr, error)
-
+    !allocate strips to each processor in z dir for speed/memory
+    allocate(tmp(0:numpoints+1, 0:numpoints+1, zi-1:zf+1))
 
     !boundary conditions
     tmp = 100.
-    tmp(0,:,:) = 0. !side face
-    tmp(:,:,0) = 0. !bottom face
-    tmp(numpoints+1,:,:) = 0. !side face
-    tmp(:,:,numpoints+1) = 0. !top face
+    tmp(xi-1,:,:) = 0. !side face
+    tmp(xf+1,:,:) = 0. !side face
+    tmp(:,yi-1,:) = 0. !front face
+    tmp(:,yf+1,:) = 0. !back face
+    if(id == numproc-1)tmp(:,:,zf+1) = 0. !top face
+    if(id == 0)tmp(:,:,zi-1) = 0. !bottom face
 
-    if(id==numproc-1)tmp(:,yf+1,:) = 0. !back face
-    if(id==0)tmp(:,0,:) = 0. !front face
+    !wrtie out init grid
+    if(id == 0)then
+        allocate(out(0:numpoints+1,0:numpoints+1, 0:numpoints+1))
+        out = 0.
 
+        do i = 1, numproc-1
+            lo = i*size_z+1
+            hi = lo + size_z-1
+            call mpi_recv(out(:,:, lo:hi), size(out(:,:,lo:hi)), mpi_real, i, tag, new_comm, recv_status, error)
+        end do
 
+        out(:,:,zi:zf) = tmp(:,:,zi:zf)
+
+        open(newunit=u,file='init_3d.dat',access='stream',form='unformatted',status='replace')
+        write(u)out
+        close(u)
+    else
+        call mpi_send(tmp(:, :, :), size(tmp(:, :,zi:zf)), mpi_real, 0, tag, new_comm, error)
+    end if
+
+    !work out # of steps for sim of tim x => $\frac{x}{\del t}$
     o = int(.1/delt)
     !do heat sim
     do p = 1, o
-        if(mod(p, 1000) == 0 .and. id == 0)print*,str(real(p)/real(o)*100.),' %'
+        if(mod(p, 1000) == 0 .and. id == 0) write(*,FMT="(A8,t21)",ADVANCE="NO") achar(13)//str(real(p)/real(o)*100., 5)//' %'
         do i = xi, xf
-            do j = yi, yf!1, n
+            do j = yi, yf
                 do k = zi, zf
                 u_xx = (tmp(i+1,j,   k)   - 2.*tmp(i,j,k) + tmp(i-1, j,   k))  * hx2
                 u_yy = (tmp(i,  j+1, k)      - 2.*tmp(i,j,k) + tmp(i,   j-1, k))  * hy2
@@ -128,41 +130,46 @@ program ring
         end do
         call mpi_barrier(new_comm, error)
 
-        !need to add subarray send here. might have wrong subarray definition though...
-
         !send_recv data to right
-        call MPI_Sendrecv(tmp(:,yf,:), size(tmp(:,yf,:)), mpi_real, right, tag, &
-                          tmp(:,yf+1,:), size(tmp(:,yf+1,:)), mpi_real, right, tag, &
+        call MPI_Sendrecv(tmp(:,:,zf), size(tmp(:,:,zf)), mpi_real, right, tag, &
+                          tmp(:,:,zf+1), size(tmp(:,:,zf+1)), mpi_real, right, tag, &
                           new_comm, recv_status, error)
         
-        !send_recv data to right
-        call MPI_Sendrecv(tmp(:,yi,:), size(tmp(:,yi,:)), mpi_real, left, tag, &
-                          tmp(:,yi-1,:), size(tmp(:,yi-1,:)), mpi_real, left, tag, &
+        !send_recv data to left
+        call MPI_Sendrecv(tmp(:,:,zi), size(tmp(:,:,zi)), mpi_real, left, tag, &
+                          tmp(:,:,zi-1), size(tmp(:,:,zi-1)), mpi_real, left, tag, &
                           new_comm, recv_status, error)
 
         call mpi_barrier(new_comm, error)
     end do
 
-    !send data to master process
-    if(id == 0)then
-        do i = 1, numproc-1
-            lo = i*size_y+1
-            hi = lo + size_y-1
-            call mpi_recv(tmp(:, lo:hi,:), size(tmp(:,lo:hi,:)), mpi_real, i, tag, new_comm, recv_status, error)
-        end do
-    else
-        call mpi_send(tmp(:, yi:yf,:), size(tmp(:, yi:yf,:)), mpi_real, 0, tag, new_comm, error)
+
+
+    if(id==0)then
+        print*,
+        call cpu_time(finish)
+        print*,finish-start
     end if
 
-    !write data out on master process
+    !send data to master process and do I/O
     if(id == 0)then
-        open(newunit=u, file='3d_parrallel_'//str(numproc)//'.dat',access='stream',form='unformatted')
-        ! do i = 0, size(tmp,1)
-        !     write(u,*)tmp(i,:)
-        ! end do
-        write(u)tmp
+        out = 0.
+        do i = 1, numproc-1
+            lo = i*size_z+1
+            hi = lo + size_z-1
+            print*,lo,hi
+            call mpi_recv(out(:,:, lo:hi), size(out(:,:,lo:hi)), mpi_real, i, tag, new_comm, recv_status, error)
+        end do
+
+        out(:,:,zi:zf) = tmp(:,:,zi:zf)
+
+        open(newunit=u, file='3d_parrallel_'//str(numproc)//'.dat',access='stream',form='unformatted',status='replace')
+        write(u)out
         close(u)
         print*,'3d_parrallel_'//str(numproc)//'.dat'
+    else
+        call mpi_send(tmp(:, :, zi:zf), size(tmp(:, :,zi:zf)), mpi_real, 0, tag, new_comm, error)
     end if
+
     call mpi_finalize(error)
 end program ring
